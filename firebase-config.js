2b30bc398fb93b6b9a8fa11e9b2f5c327093e351
King-Jack21 <rinaeve18@gmail.com>
2026-06-11 22:29:38 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+// 🔥 CUKUP INI AJA, TANPA API KEY! 🔥
+const firebaseConfig = {
+    databaseURL: "https://dikxrat-default-rtdb.asia-southeast1.firebasedatabase.app"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
